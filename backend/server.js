7516f4c775ebfc7c7fd02e6656e170bb438ad69d
MatamaPeter <peter24matama@gmail.com>
2025-03-11 18:23:11 +0300

    created the authoristion routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js"
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send('Hello world!')
-});
+dotenv.config();
+const PORT = process.env.PORT || 5000 ;
 
-app.listen(5000, ()=>console.log("Server running on port 5000"))
+
+app.get("/", (req, res) => {
+    res.send('Hello world')
+})
+
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
